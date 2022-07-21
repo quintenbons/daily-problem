97e73b396cc237ca66bbcae0b54f29c46b7b63bc
quintenbons <quintenbons@outlook.com>
2022-07-21 23:45:13 +0200

    problem6

---
+++ src/problem6.rs
@@ -0,0 +1,4 @@
+/// # Hard - Google
+/// Xor list. Sadly this is only for C. Implementing this
+/// in rust is just not meant to be. It is still possible
+/// with refcells, bout out of the scope of this training
