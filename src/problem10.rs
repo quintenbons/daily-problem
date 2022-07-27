8a8e0e2a56b0f7bb161ffeeff77b3b1dd74ee4fb
quintenbons <quintenbons@outlook.com>
2022-07-27 03:08:04 +0200

    problem10

--- src/problem10.rs
+++ src/problem10.rs
@@ -18,10 +18,110 @@ mod tests {
 
     #[test]
     fn job_thread() {
-        thread_manager();
+        naive_scheduler();
+    }
+
+    #[test]
+    fn heap_thread() {
+        heap_scheduler();
     }
 }
 
+struct BinaryHeap(Vec<Job>);
+
+impl BinaryHeap {
+    fn new() -> BinaryHeap {
+        BinaryHeap(Vec::new())
+    }
+
+    fn push(&mut self, job: Job) {
+        let vec = &mut self.0;
+
+        vec.push(job);
+
+        // correct heap
+        let mut i = vec.len()-1;
+
+        while i != 0 {
+            // get parent
+            let j = (i - 1) / 2;
+
+            // done
+            if vec[i].when >= vec[j].when {
+                return;
+            }
+
+            // switch
+            vec.swap(i, j);
+            i = j;
+        }
+    }
+
+    fn peek(&self) -> Option<&Job> {
+        self.0.get(0)
+    }
+
+    /// # Panics
+    ///
+    /// Panics if `BinaryHeap` is empty.
+    fn pop(&mut self) -> Job {
+        let vec = &mut self.0;
+
+        let res = vec.swap_remove(0);
+
+        // correct heap
+        let mut i = 0;
+
+        while i == 0 {
+            // get children
+            let left = i * 2 + 1;
+            let right = i * 2 + 2;
+
+            match (vec.get(left), vec.get(right)) {
+                (None, None) => break,
+                (Some(job_left), None) => {
+                    if job_left.when < vec[i].when {
+                        vec.swap(i, left);
+                        i = left;
+                    } else {
+                        break;
+                    }
+                }
+                (None, Some(job_right)) => {
+                    if job_right.when < vec[i].when {
+                        vec.swap(i, right);
+                        i = right;
+                    } else {
+                        break;
+                    }
+                }
+                (Some(job_left), Some(job_right)) => {
+                    // check if we need a switch
+                    let inst = vec[i].when;
+                    if job_right.when >= inst && job_left.when >= inst {
+                        break;
+                    }
+
+                    if job_left.when < job_right.when {
+                        vec.swap(i, left);
+                        i = left;
+                    } else {
+                        vec.swap(i, right);
+                        i = right;
+                    }
+                }
+            }
+        }
+
+        res
+    }
+
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+#[derive(Debug)]
 struct Job {
     todo: fn(),
     when: Instant,
@@ -70,7 +170,7 @@ fn thread_naive() {
 /// Use one thread that launches jobs. This is very performance hungry,
 /// we could use system clock interuptions instead. This is somewhat
 /// similar to JS's async approach, but JS is single threaded.
-fn thread_manager() {
+fn naive_scheduler() {
     let (tx, rx) = mpsc::channel::<Option<Job>>();
     let txx = tx.clone();
 
@@ -124,6 +224,80 @@ fn thread_manager() {
     delay_job(func2, 1030);
     delay_job(func1, 1000);
     delay_job(func2, 900);
+    terminate();
+
+    job_manager.join().expect("Waiting for job thread");
+
+    println!("DONE");
+}
+
+/// The only difference with the previous solution is the use of
+/// a heap, which makes this A lot more efficient
+fn heap_scheduler() {
+    let (tx, rx) = mpsc::channel::<Option<Job>>();
+    let txx = tx.clone();
+
+    fn manage_jobs(rx: mpsc::Receiver<Option<Job>>) {
+        let mut jobs = BinaryHeap::new();
+        let mut terminate = false;
+
+        while !terminate || jobs.len() > 0 {
+            let next_job = jobs.peek();
+
+            let new_job = if let None = next_job {
+                // We are not expecting a job, so we can wait
+                Some(rx.recv().unwrap())
+            } else {
+                // We are expecting a job, we can wait only for so long
+                let when = next_job.unwrap().when;
+                let wait_time = when - Instant::now();
+
+                if let Ok(new_job) = rx.recv_timeout(wait_time) {
+                    Some(new_job)
+                } else {
+                    None
+                }
+            };
+
+            // We received a message
+            if let Some(new_job) = new_job {
+                if let Some(new_job) = new_job {
+                    // It is a job
+                    jobs.push(new_job);
+                } else {
+                    // It is a terminate signal
+                    terminate = true;
+                }
+                continue;
+            }
+
+            // The job must be executed now
+            let job = jobs.pop();
+            (job.todo)();
+        }
+    }
+
+    let job_manager = thread::spawn(move || {
+        manage_jobs(rx);
+    });
+
+    let delay_job = move |f: fn(), n: u64| {
+        txx.send(Some(Job::new(f, n)))
+            .expect("Send job to side thread");
+    };
+
+    let terminate = move || {
+        tx.send(None).expect("Sending a terminate signal");
+    };
+
+    // now it works flawlesly 212121
+    delay_job(func2, 1001);
+    delay_job(func1, 1000);
+    delay_job(func1, 1002);
+    delay_job(func2, 999);
+    delay_job(func2, 900);
+    thread::sleep(Duration::from_millis(600));
+    delay_job(func1, 301); // total will be 901
     terminate();
 
     job_manager.join().expect("Waiting for job thread");
