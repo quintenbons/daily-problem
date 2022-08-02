1fae1113f2ff2b7e7b83cc4b8e3e06ca39696c35
quintenbons <quintenbons@outlook.com>
2022-08-02 17:44:38 +0200

    problem16

--- src/problem16.rs
+++ src/problem16.rs
@@ -11,20 +11,25 @@ mod tests {
         let mut order_list = OrderList::new(3);
         order_list.record(1);
         order_list.record(2);
-        order_list.record(3);
+
+        assert_eq!(order_list.get_last(0), &Some(2));
+        assert_eq!(order_list.get_last(1), &Some(1));
+        assert_eq!(order_list.get_last(2), &None);
+        assert_eq!(order_list.get_last(3), &None);
+
         order_list.record(4);
         order_list.record(5);
 
-        assert_eq!(order_list.get_last(0), Some(5));
-        assert_eq!(order_list.get_last(0), Some(4));
-        assert_eq!(order_list.get_last(0), Some(3));
-        assert_eq!(order_list.get_last(0), None);
+        assert_eq!(order_list.get_last(0), &Some(5));
+        assert_eq!(order_list.get_last(1), &Some(4));
+        assert_eq!(order_list.get_last(2), &Some(2));
+        assert_eq!(order_list.get_last(3), &None);
     }
 }
 
 struct OrderList {
     index: usize,
-    log: Vec<Option<i32>>
+    log: Vec<Option<i32>>,
 }
 
 impl OrderList {
@@ -37,11 +42,17 @@ impl OrderList {
 
     /// Adds an order to the list
     fn record(&mut self, order_id: i32) {
-        unimplemented!();
+        self.log[self.index] = Some(order_id);
+        self.index = (self.index + 1) % self.log.len();
     }
 
     /// Gets the ith last element from the list
-    fn get_last(&self, i: usize) -> Option<i32> {
-        unimplemented!();
+    fn get_last<'a>(&'a self, i: usize) -> &'a Option<i32> {
+        if i < self.log.len() {
+            let key = (self.index + self.log.len() - i - 1) % self.log.len();
+            &self.log[key]
+        } else {
+            &None
+        }
     }
 }
