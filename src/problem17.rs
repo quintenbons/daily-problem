e7533348cb7528dfa8eb235a65749eae92f548a9
quintenbons <quintenbons@outlook.com>
2022-08-02 20:13:46 +0200

    problem17

--- src/problem17.rs
+++ src/problem17.rs
@@ -15,15 +15,16 @@ mod tests {
     }
 }
 
+use std::cmp::Ordering;
 use std::iter::Peekable;
 
 enum Element {
-    File { size: u32 },
-    Dir { size: u32, children: Vec<Element> },
+    File { size: usize },
+    Dir { size: usize, children: Vec<Element> },
 }
 
 /// Gives the length of the longest path of the filesystem
-fn get_longest(fs: &str) -> u32 {
+fn get_longest(fs: &str) -> usize {
     let root = parse_fs(fs);
 
     match root {
@@ -35,15 +36,79 @@ fn get_longest(fs: &str) -> u32 {
 /// Parses the fs to an Element, making it easier to
 /// navigate, since we don't care about the strings
 fn parse_fs(fs: &str) -> Option<Element> {
-    unimplemented!();
+    fn to_element(path: &str) -> Element {
+        let real_name = path.trim_start_matches('\t');
+        if real_name.contains('.') {
+            Element::File {
+                size: real_name.len(),
+            }
+        } else {
+            Element::Dir {
+                size: real_name.len(),
+                children: Vec::new(),
+            }
+        }
+    }
+
+    fn aux<'a, I>(
+        iter: &mut Peekable<I>,
+        current_depth: usize,
+        siblings: &mut Vec<Element>,
+    ) where
+        I: Iterator<Item = &'a str>,
+    {
+        loop {
+            // stop at the end of filesystem
+            let next = match iter.peek() {
+                Some(n) => n,
+                None => return,
+            };
+
+            let depth = next.chars().take_while(|c| *c == '\t').count();
+
+            match depth.cmp(&current_depth) {
+                Ordering::Greater => panic!("Invalid filesystem"),
+
+                // this dir is done
+                Ordering::Less => return,
+
+                // another sibling
+                Ordering::Equal => {
+                    let mut element = to_element(next);
+                    iter.next();
+
+                    if let Element::Dir { children, .. } = &mut element {
+                        aux(iter, current_depth + 1, children);
+                    }
+
+                    siblings.push(element);
+                }
+            }
+        }
+    }
+
+    if fs.is_empty() {
+        return None;
+    }
+
+    let mut iter = fs.split('\n').peekable();
+    let mut res = to_element(iter.next().unwrap());
+
+    match &mut res {
+        Element::File { .. } => Some(res),
+        Element::Dir { children, .. } => {
+            aux(&mut iter, 1, children);
+            Some(res)
+        }
+    }
 }
 
 /// Looks for the longest paths from an Element
-fn longest_sum(elm: &Element) -> u32 {
+fn longest_sum(elm: &Element) -> usize {
     match elm {
         Element::File { size } => *size,
         Element::Dir { size, children } => {
-            size + children
+            size + 1 + children
                 .iter()
                 .map(|elm| longest_sum(elm))
                 .max()
