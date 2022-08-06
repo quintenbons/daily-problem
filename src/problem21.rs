ad4e0423d6150d1f0675411f1146ee88ce60b0c7
quintenbons <quintenbons@outlook.com>
2022-08-06 15:12:41 +0200

    problem21 structure

--- src/problem21.rs
+++ src/problem21.rs
@@ -1,3 +1,24 @@
 #![allow(dead_code)]
 //! Easy - Snapchat
 //! Room management in a school
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let lectures0 = vec![(30, 75), (0, 50), (60, 150)];
+        let lectures1 =
+            vec![(30, 75), (0, 50), (60, 150), (50, 60), (80, 130), (90, 130)];
+
+        assert_eq!(min_rooms(&lectures0), 2);
+        assert_eq!(min_rooms(&lectures1), 3);
+    }
+}
+
+/// From a vector of lecture start and stop points
+/// find the minimum amount of rooms needed
+fn min_rooms(lectures: &Vec<(u32, u32)>) -> u32 {
+    unimplemented!();
+}
