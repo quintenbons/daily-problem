932d2cc7f0bd32e3bdb29c065fe383bba6fa29e5
quintenbons <quintenbons@outlook.com>
2022-08-07 19:16:07 +0200

    problem22

--- src/problem22.rs
+++ src/problem22.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 //! Medium - Microsoft
 //! Untangle a sentence without spaces
+//! I admit my test is not very exhaustive
+//! but this problem is hard to test thoroughly
 
 #[cfg(test)]
 mod tests {
@@ -16,6 +18,7 @@ mod tests {
             "the".to_string(),
             "ran".to_string(),
         ];
+
         assert_eq!(
             untangle(&dictionary, "quickfastbrownfoxranfast"),
             vec!["quick", "fast", "brown", "fox", "ran", "fast"]
@@ -23,6 +26,12 @@ mod tests {
     }
 }
 
+/// There are many ways to solve this in O(N^2). We could also do as
+/// a previous problem, and make a dictionary out of dic: letter -> dic
+/// to save up some time for multiple calls of the function. That would
+/// take up (n*max common prefix) time to make the dictionary + O(N) recursions
+/// per word of the sentence at worst. Meaning still a O(NxM), but way more
+/// performant. I think the implementation here is not interesting
 fn untangle<'a>(dictionary: &Vec<String>, sentence: &str) -> Vec<&'a str> {
     unimplemented!();
 }
