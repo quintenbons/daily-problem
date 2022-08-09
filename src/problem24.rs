ec59de2998f47ffe1d14593908aea174e1d341ed
quintenbons <quintenbons@outlook.com>
2022-08-09 22:51:05 +0200

    problem24

--- src/problem24.rs
+++ src/problem24.rs
@@ -1,4 +1,4 @@
-#[allow(dead_code)]
+#![allow(dead_code)]
 //! Medium - Google
 //! Locked binary trees
 //! No test this time
@@ -11,7 +11,7 @@ struct Node {
     locked_children: usize,
     child0: Rc<Tree>,
     child1: Rc<Tree>,
-    parent: Weak<Tree>,
+    parent: Option<Weak<Tree>>,
 }
 
 enum Tree {
@@ -19,22 +19,77 @@ enum Tree {
     Node(Node),
 }
 
-impl Tree {
+impl Node {
     fn is_locked(&self) -> bool {
-        unimplemented!();
+        self.locked
     }
 
     fn lock(&mut self) -> bool {
-        unimplemented!();
+        if !self.locked && self.can_toggle() {
+            self.locked = true;
+            self.increment_ancestors();
+            true
+        } else {
+            false
+        }
     }
 
     /// If you lock both father and son
     /// they will be locked forever...
     fn unlock(&mut self) -> bool {
-        unimplemented!();
+        if self.locked && self.can_toggle() {
+            self.locked = false;
+            self.decrement_ancestors();
+            true
+        } else {
+            false
+        }
     }
 
     fn can_toggle(&self) -> bool {
-        unimplemented!();
+        self.locked_children == 0 && !self.locked_ancestor()
+    }
+
+    fn locked_ancestor(&self) -> bool {
+        match &self.parent {
+            None => false,
+            Some(weak_tree) => {
+                if let Tree::Node(node) =
+                    Rc::get_mut(&mut weak_tree.upgrade().unwrap()).unwrap()
+                {
+                    node.locked || node.locked_ancestor()
+                } else {
+                    panic!("Structural error in the tree");
+                }
+            }
+        }
+    }
+
+    fn increment_ancestors(&mut self) {
+        match &self.parent {
+            None => (),
+            Some(weak_tree) => {
+                if let Tree::Node(node) =
+                    Rc::get_mut(&mut weak_tree.upgrade().unwrap()).unwrap()
+                {
+                    node.locked_children += 1;
+                    node.increment_ancestors();
+                }
+            }
+        }
+    }
+
+    fn decrement_ancestors(&mut self) {
+        match &self.parent {
+            None => (),
+            Some(weak_tree) => {
+                if let Tree::Node(node) =
+                    Rc::get_mut(&mut weak_tree.upgrade().unwrap()).unwrap()
+                {
+                    node.locked_children -= 1;
+                    node.increment_ancestors();
+                }
+            }
+        }
     }
 }
